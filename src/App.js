ef7b51aab6c3f9d43d072e2109e6ae1a0216a66c
monika <monika>
2022-12-12 16:13:05 +0545

    review page done

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import './App.css';
 import Home from './pages/HomePage/homepage'
 import Servicepage from './pages/ServicesPage/servicepage';
 import Providerpage from './pages/ProvidersPage/providerpage'
+import Reviewpage from './pages/ReviwePage/reviewpage'
 function App() {
   return (
    <>
    <div className='container'>
-    <Home/>
+    {/* <Home/>
    <Servicepage/>
-   <Providerpage/>
+   <Providerpage/> */}
+   <Reviewpage/>
    </div>
 
    </>
---
+++ src/pages/ReviwePage/reviewpage.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import './reviwepage.css'
+import userimg from '../../assest/userImg.png'
+import arrownav from '../../assest/arrownav.png'
+export default function reviewpage() {
+  return (
+    <>
+    <div className='reviewpage-content-warrper'>
+    
+    <div className='reviewpage-box'>
+        <h4>What our customer are saying</h4>
+        <div className='row user-detalis-holder'>
+            <div className='col-6'>
+                <ul >
+                    <li><img src={userimg}/></li>
+                    <li className='user-data'><p className='user-name'>Edward Newgate</p>
+                        <span className='user-postion'>
+                        Founder Circle
+                        </span>
+                    </li>
+                </ul>
+            </div>
+
+            <div className='col-6'>
+                <p className='user-reviwe'>
+                “Our dedicated patient engagement app
+                <br/> and 
+                 web portal allow you to access information
+                 <br/> instantaneously (no tedeous form, long calls, <br/>
+                 or administrative hassle) and securely”
+                </p>
+            </div>
+
+        </div> 
+           
+    </div>
+    <div className='arrownav-img'><img src={arrownav}/></div>
+
+    </div>
+    
+    </>
+  )
+}
